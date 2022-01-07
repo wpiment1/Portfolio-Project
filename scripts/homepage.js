6985347ad6eefd15c89520c05508760336a03cc4
wpiment1 <winifer.pimentel@gmail.com>
2022-01-07 18:41:38 -0500

    fixed cursor in typewriting effect

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,11 +1,14 @@
-new TypeIt('.type1', {
+new TypeIt(".type1", {
   startDelay: 900,
   strings: "im winifer pimentel",
-  speed: 50,
+  speed: 80,
+  afterComplete: function (step, instance) {
+    instance.destroy();
+  },
 }).go();
 
 new TypeIt(".type2", {
-  startDelay: 2000,
+  startDelay: 2500,
   strings: ["an aspiring front-end developer based in new york city"],
   speed: 50,
 }).go();
