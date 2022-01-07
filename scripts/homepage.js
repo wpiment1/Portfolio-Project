6f8e5a800489c9819c3474f5fc7c3f5feb56bb16
wpiment1 <winifer.pimentel@gmail.com>
2022-01-07 18:12:29 -0500

    replaced typewriting effect

---
+++ scripts/homepage.js
@@ -0,0 +1,11 @@
+new TypeIt('.type1', {
+  startDelay: 900,
+  strings: "im winifer pimentel",
+  speed: 50,
+}).go();
+
+new TypeIt(".type2", {
+  startDelay: 2000,
+  strings: ["an aspiring front-end developer based in new york city"],
+  speed: 50,
+}).go();
